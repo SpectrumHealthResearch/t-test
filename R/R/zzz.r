1454f7612d5d30290412b07571a0bda61b517ca2
Paul W. Egeler, MS, GStat <paul.egeler@spectrumhealth.org>
2019-06-26 16:17:18 -0400

    Changed stt.r to zzz.r to match pkg dev conventions

--- R/R/zzz.r
+++ R/R/zzz.r
@@ -4,7 +4,7 @@
       "This is a development version of `stt`",
       "======================================",
       "Functionality is incomplete:",
-      "  - Only `method = 'satterthwaite'` is supported",
+      "  - Only pooled and Satterthwaite methods are supported",
       "  - Calculation outputs have not been unit tested",
       "Please be sure to review all calculations for accuracy",
       "since this is provided AS IS with NO WARRANTY!",
